6daffd2612b979431861fc4af57056a8e0e6325d
Marin <marinmema@live.ca>
2025-01-26 09:19:49 -0500

    add qr code

--- script.js
+++ script.js
@@ -1,51 +1,36 @@
-const channel = (new URL(window.location)).searchParams.get('channel') ?? '';
-const SERVER_URL = `https://marinm.net/broadcast?channel=${channel}`;
+const initialChannel =
+    new URL(window.location).searchParams.get("channel") ?? null;
+const channel = initialChannel ?? randomID();
+const BROADCAST_URL = `https://marinm.net/broadcast?channel=${channel}`;
+const SHARE_URL = `https://marinm.net/rocket/?channel=${channel}`;
 
 let webSocket = null;
 let connectedAt = null;
 let tappedAt = null;
 
-connect();
-
 const container = document.getElementById("container");
 const bigHeart = document.getElementById("big-heart");
 const square = document.getElementById("square");
-const qrcodeContainer = document.getElementById("qrcode-container");
 const randomNumber = document.getElementById("random-number");
+const popup = document.getElementById("popup");
+const qrcodeContainer = document.getElementById("qrcode-container");
 
 const animations = ["float-1", "float-2", "float-3", "float-4"];
 
 let timeoutId = null;
 
+connect();
 
-function randomID() {
-    const array = new Uint32Array(1);
-    self.crypto.getRandomValues(array);
+popup.style.visibility = "hidden";
 
-    const result = array[0].toString().padStart(10, '0').slice(1);
+square.addEventListener("click", openPopup);
 
-    const segments = [
-        result.slice(0, 3),
-        result.slice(3, 6),
-        result.slice(6, 9),
-    ];
-
-    return segments.join('-');
-}
-
-square.addEventListener("click", () => {
-    qrcodeContainer.innerText = randomID();
+const qrcode = new QRCode(qrcodeContainer, {
+    width: qrcodeContainer.clientWidth,
+    height: qrcodeContainer.clientWidth,
 });
 
-function growHeart() {
-    bigHeart.classList.add("tapped");
-    if (timeoutId) {
-        clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-        bigHeart.classList.remove("tapped");
-    }, 250);
-}
+qrcode.makeCode(SHARE_URL);
 
 bigHeart.addEventListener("click", () => {
     tappedAt = Date.now();
@@ -62,6 +47,36 @@ bigHeart.addEventListener("click", () => {
     webSocket.send(randomAnimation());
 });
 
+function randomID() {
+    const array = new Uint32Array(1);
+    self.crypto.getRandomValues(array);
+
+    const result = array[0].toString().padStart(10, "0").slice(1);
+
+    const segments = [
+        result.slice(0, 3),
+        result.slice(3, 6),
+        result.slice(6, 9),
+    ];
+
+    return segments.join("-");
+}
+
+function openPopup() {
+    popup.style.visibility =
+        popup.style.visibility === "hidden" ? "visible" : "hidden";
+}
+
+function growHeart() {
+    bigHeart.classList.add("tapped");
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        bigHeart.classList.remove("tapped");
+    }, 250);
+}
+
 function randomAnimation() {
     return animations[Math.floor(Math.random() * animations.length)];
 }
@@ -77,7 +92,7 @@ function newHeart(animation) {
 }
 
 function connect() {
-    webSocket = new WebSocket(SERVER_URL);
+    webSocket = new WebSocket(BROADCAST_URL);
 
     webSocket.addEventListener("open", () => {
         console.log("open");
